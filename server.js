ec6ba8a5af0962f8eba58f0c30c395e5a0878c91
Munish Rana <sam786rana786@gmail.com>
2018-12-07 17:03:56 +0530

    home rout updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {res.send(database.users)});
+app.get('/', (req, res)=> {res.send('it is working!')});
 app.post('/signin', signin.handlesignin(db, bcrypt));
 app.post('/register', register.handleregister(db, bcrypt));
 app.get('/profile/:id', profile.handleprofile(db));
